1f55713a3e72802716be90adf447c8a4ee35dfb0
Andrii Popov <popov.andrey80@gmail.com>
2022-12-24 19:31:57 +0200

    correct smth

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,9 +4,11 @@ import css from './Contact.module.css';
 function Contact({ contact: { name, number, id }, onDeleteContact }) {
   return (
     <div className={css.contactWrapper}>
-      <p className={css.contactData}>
-        {name}: {number}
-      </p>
+      <>
+        <p className={css.contactData}>
+          {name}: {number}
+        </p>
+      </>
       <button className={css.deleteContactBtn} type="button" onClick={() => onDeleteContact(id)}>
         Delete contact
       </button>
